d497f808381c875bbd92745d29cf8492ff4dbcc3
TolyNoriega5 <125397518+TolyNoriega5@users.noreply.github.com>
2025-10-08 00:28:25 -0500

    chore: initial commit

---
+++ src/test/java/com/upc/pre/peaceapp/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.pre.peaceapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
